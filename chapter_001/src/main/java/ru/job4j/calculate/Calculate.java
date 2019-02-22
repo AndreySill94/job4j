8febd739215f08aa340d26e591aa810a643601d1
Andrey Silichev <sill.av.1994@gmail.com>
2019-02-22 16:10:54 +0300

    Добавление JavaDoc

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,14 @@
+/**
+ * Calculate
+ * @author Andrey Silichev (sill.av.1994@gmail.com)
+ */
+
 package ru.job4j.calculate;
 
+/**
+ * Main
+ * @param args - args.
+ */
 public class Calculate {
     public static void main(String[] args) {
         System.out.println("Hello World!");
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,9 @@
+/**
+* Package for calculate task
+*
+* @author Silichev Andrey (sill.av.1994@gmail.com)
+* @version $id$
+* @since 0.1
+*/
+
+package ru.job4j.calculate
